5a5d076afa75b047682f6cd7adbf89a134654a66
Mushafa Andira <>
2024-07-14 20:27:47 +0700

    convert H:m to HH:mm format if the number is one digit

--- lib/src/scroll_time_picker.dart
+++ lib/src/scroll_time_picker.dart
@@ -194,7 +194,8 @@ void _init12hFormat() {
   void _initTimeScrollView() {
     _hourScrollView = TimeScrollView(
         key: const Key('hour'),
-        times: _hours,
+        /// change hours to string so it can be formated to HH instead of H
+        times: _hours.map((hour)=> hour.toString().padLeft(2, '0')).toList(),
         controller: _hourController,
         options: widget.options,
         scrollViewOptions: widget.scrollViewOptions.hour,
@@ -205,7 +206,8 @@ void _init12hFormat() {
         });
     _minuteScrollView = TimeScrollView(
         key: const Key('minute'),
-        times: _minutes,
+        /// change minutes to string so it can be formated to mm instead of m
+        times: _minutes.map((minute)=> minute.toString().padLeft(2, '0')).toList(),
         controller: _minuteController,
         options: widget.options,
         scrollViewOptions: widget.scrollViewOptions.minute,
